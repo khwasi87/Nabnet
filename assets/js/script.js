cde608de94a5ce4cdd9229a1a09551b21906ed2e
Khan Wasim <khwasi.87@gmail.com>
2021-04-20 12:02:10 +0530

    js and css update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
     $('#accordion .accordion-body').hide();
     $('#accordion .accordion-header span').click(function() {
-        $('.fa-angle-down').toggleClass('.fa-angle-up');
         $(this).parent().next().slideToggle()
             .siblings('.accordion-body').slideUp();
         return false;
